1bdcb12132c0414a674a9a5bd1e924be33d8294b
NehemiasEC <neli92santscz@gmail.com>
2020-06-25 21:54:49 -0400

    master

--- backend/env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/nehemiasec/projects/fullstack/buildingstatux/backend/env/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
